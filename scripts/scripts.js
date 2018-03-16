d4c547b7c92704a9df243d23437526b4aa0e08f0
danielhkim92 <danielhkim92@gmail.com>
2018-03-16 16:10:27 -0500

    finished skills section now working on about section"

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 $('.button-collapse').sideNav({
   menuWidth: 250, // Default is 300
   edge: 'left', // Choose the horizontal origin
-  closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
   draggable: true // Choose whether you can drag to open on touch screens
 });
 
