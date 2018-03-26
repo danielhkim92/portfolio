0e5993af6ee520c071f85ab096b2d15711f299a6
danielhkim92 <danielhkim92@gmail.com>
2018-03-26 17:12:29 -0500

    finished adding transitions. cant add zoom animation with scrolling animation so added zoom class to imgs instead of the li or ahref

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -25,9 +25,23 @@ $('.collapsible').collapsible();
 
   // Or with jQuery
 
-window.sr = ScrollReveal();
+window.sr = ScrollReveal({ duration: 1500 });
+sr.reveal('.scrollanimation', 300);
+sr.reveal('.scrollanimationhome', 300);
+sr.reveal('.scrollanimationfooter', 200);
+sr.reveal('.skillscrollanimation', 200);
+sr.reveal('.skill2scrollanimation', 200);
+sr.reveal('.skill3scrollanimation', 200);
+sr.reveal('.skill4scrollanimation', 200);
+sr.reveal('.projectskill1scrollanimation', 100);
+sr.reveal('.projectskill2scrollanimation', 100);
+sr.reveal('.projectskill3scrollanimation', 100);
 
+sr.reveal('.aboutscrollanimation', { duration: 1000 });
 
+sr.reveal('.aboutcontentscrollanimation', 400);
+sr.reveal('.skillsscrollanimation', 1500);
+sr.reveal('.projectsscrollanimation', 1500);
 // Customizing a reveal set
-sr.reveal('.row', { duration: 900 });
-sr.reveal('#projects', { duration: 900 });
+
+
