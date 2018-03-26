45896a13b89c2a9595d5dc9c0fd900f7e0c17cd0
danielhkim92 <danielhkim92@gmail.com>
2018-03-26 16:20:08 -0500

    added tool-tips

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -20,3 +20,11 @@ $("#js-rotating").Morphext({
 $(".button-collapse").sideNav();
 // Initialize collapsible (uncomment the line below if you use the dropdown variation)
 $('.collapsible').collapsible();
+
+
+
+  // Or with jQuery
+
+$(document).ready(function(){
+  $('.tooltipped').tooltip();
+});
